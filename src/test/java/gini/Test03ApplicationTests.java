33ebb46049712809a44574c52524df38480c8da2
iordache <mihai.iordache82@gmail.com>
2024-06-22 17:12:45 +0300

    first commit

---
+++ src/test/java/gini/Test03ApplicationTests.java
@@ -0,0 +1,13 @@
+package gini;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Test03ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
